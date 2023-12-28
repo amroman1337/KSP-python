bcb6058d849cd6a26edb2c94a6ba3f96dcf3204c
Roman Matyushkin <144627459+amroman1337@users.noreply.github.com>
2023-12-29 00:39:51 +0300

    Update Apollo11_speed.py

--- Apollo11_speed.py
+++ Apollo11_speed.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt  # библиотека для рисования графиков
-import krpc  # библиотека для подключения к KSP и забора данных
-
-connection = krpc.connect(name='Graphics')
+import krpc  # библиотека для подключения к KSP и сбора данных
+import time
+import datetime
+from scipy.integrate import odeint
+ 
 vessel = connection.space_center.active_vessel
-
 figure, ax = plt.subplots()
 Speed = []
 Time = []
@@ -16,9 +17,11 @@ while True:
     if vessel.metvessel.flight(Orbit).speed > 1000:
         break
 
+
 # Работаем с графиком
 ax.plot(Speed, Time)  # рисуем график
 plt.xlabel('Время, сек')
 plt.ylabel('Скорость, м/с')
 plt.grid(color='black')  # наводим красоту
 plt.show()
+
