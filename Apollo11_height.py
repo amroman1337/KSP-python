2fc9768ad86620de4f39978492ad13de71cc35e7
Roman Matyushkin <144627459+amroman1337@users.noreply.github.com>
2023-12-29 00:38:30 +0300

    Update Apollo11_height.py

--- Apollo11_height.py
+++ Apollo11_height.py
@@ -1,25 +1,27 @@
-import time
-import datetime
-from scipy.integrate import odeint
-import krpc
+ import matplotlib.pyplot as plt  # библиотека для рисования графиков
+ import krpc  # библиотека для подключения к KSP и сбора данных
+ import time
+ import datetime
+ from scipy.integrate import odeint
 
-connection = krpc.connect(name='Graphics')
-vessel = connection.space_center.active_vessel
-figure, ax = plt.subplots()
-Time = []
-Speed = []
-Orbit = vessel.orbit.body.reference_framesaved = 999
-time.sleep(4)
+ connection = krpc.connect(name='Graphics')
+ vessel = connection.space_center.active_vessel
+ figure, ax = plt.subplots()
+ Time = []
+ Height = []
+ Orbit = vessel.orbit.body.reference_framesaved = 999
+ time.sleep(4)
 
-# Получаем данные
-while True:
-    Speed.append(vessel.flight(Orbit).mean_altitude)
-    Time.append(vessel.met)
-    if vessel.met > 85:
-        break
+ # Получаем данные
+ while True:
+ 	Height.append(vessel.flight(Orbit).mean_altitude)
+ 	Time.append(vessel.met)
+ 	if vessel.met > 85:
+     	break
 
-# Строим график
-ax.plot(Time, Speed)
-plt.ylabel('высота, м')
-plt.grid(color='black')  
-plt.show()
+ # Строим график
+ ax.plot(Time, Speed)
+ plt.xlabel('Время, сек')
+ plt.ylabel('высота, м')
+ plt.grid(color='black') 
+ plt.show()
